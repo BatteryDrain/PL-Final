81747b023b0f2a336c7f29052c138f99f7f3150e
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-04-14 12:26:49 -0600

    added hero img

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiURL = "https://api.weatherapi.com/v1/current.json?key=4e5e576e864142e780a45600252403&q=calgary";
+    fetch(apiURL)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error("Network response was not OK");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            var weatherInfo = data;
+            city.innerHTML = weatherInfo.location.name;
+            tempe.innerHTML = "Temperature: " + weatherInfo.current.temp_c + "°C";
+            flike.innerHTML = "feels like: " + weatherInfo.current.feelslike_c + "°C";
+            icon.src = "https:" + weatherInfo.current.condition.icon;
+            description.innerHTML= weatherInfo.current.condition.text;
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
