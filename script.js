1307fbc18bc54893ccfc7a311f0c1904adf2abcd
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-04-15 02:46:45 -0600

    added weather

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.weatherapi.com/v1/current.json?key=4e5e576e864142e780a45600252403&q=calgary";
+const apiURL = "https://api.weatherapi.com/v1/current.json?key=4e5e576e864142e780a45600252403&q=Cancun";
     fetch(apiURL)
         .then(response => {
             if(!response.ok) {
@@ -9,11 +9,9 @@ const apiURL = "https://api.weatherapi.com/v1/current.json?key=4e5e576e864142e78
         .then(data => {
             console.log(data);
             var weatherInfo = data;
-            // city.innerHTML = weatherInfo.location.name;
-            // tempe.innerHTML = "Temperature: " + weatherInfo.current.temp_c + "°C";
-            // flike.innerHTML = "feels like: " + weatherInfo.current.feelslike_c + "°C";
-            // icon.src = "https:" + weatherInfo.current.condition.icon;
-            // description.innerHTML= weatherInfo.current.condition.text;
+            tempe.innerHTML = "Temperature: " + weatherInfo.current.temp_c + "°C";
+            icon.src = "https:" + weatherInfo.current.condition.icon;
+            description.innerHTML= weatherInfo.current.condition.text;
         })
         .catch(error => {
             console.error("Error:", error);
